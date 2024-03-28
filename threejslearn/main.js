a164ff0c7e47ea456baaee713fa0fec62227f452
StormRobert <stormarasa2@gmail.com>
2024-03-28 14:53:07 +0300

    added controls of sphere and resizing of the canvas

--- threejslearn/main.js
+++ threejslearn/main.js
@@ -1,4 +1,6 @@
 import * as THREE from 'three';
+import './style.css'
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
 // Scene
 const scene = new THREE.Scene();
 
@@ -11,13 +13,19 @@ const material = new THREE.MeshStandardMaterial({
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh)
 
+//Sizes of the screen
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+}
+
 //light
 const light = new THREE.PointLight(0xFFFFFF, 70, 100, 1.7 );
 light.position.set(0, 10, 10) // x, y, z respectively
 scene.add(light)
 
 // Camera
-const camera = new THREE.PerspectiveCamera(45, 800 / 600 , 0.1, 100) 
+const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height , 0.1, 100) 
 //the 0.1 and 100 is for the camera position at z if it goes past 0.1 or 100 the sphere will not show
 camera.position.z = 20;
 scene.add(camera)
@@ -25,5 +33,33 @@ scene.add(camera)
 //Renderer
 const canvas = document.querySelector( '.webgl' );
 const renderer = new  THREE.WebGLRenderer({canvas});
-renderer.setSize(800, 600)
+renderer.setSize(sizes.width, sizes.height)
 renderer.render(scene, camera)
+
+//controls for  moving around the sphere
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true;
+controls.enablePan = false;
+controls.enableZoom = false;
+
+//Resizing
+window.addEventListener('resize', () =>{
+  // Update sizes
+  console.log(window.innerWidth, window.innerHeight)
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  //Update camera
+  //make sure to update the camera for it to be in sync
+  camera.aspect = sizes.width/sizes.height;
+  camera.updateProjectionMatrix(); // helps with the aspect ratio of the sphere
+  renderer.setSize(sizes.width, sizes.height)
+})
+
+// rerender the canvas so that the sphere is always kept updated as it changes the screen ratio
+const loop = () => {
+  controls.update()
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(loop);
+}
+loop()
